42ce0683da0a8d4907391f37edfe8b544c196dc9
Pavel Kraus <pavel.kraus01@gmail.com>
2023-10-15 16:49:12 +0200

    Data Access Layer skeleton, first example entity (User)

---
+++ DataAccessLayer/BookHubDbContext.cs
@@ -0,0 +1,26 @@
+﻿using BookHub.DataAccessLayer.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookHub.DataAccessLayer;
+
+public class BookHubDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        const Environment.SpecialFolder folder = Environment.SpecialFolder.LocalApplicationData;
+        var dbPath = Path.Join(Environment.GetFolderPath(folder), "BookHub.sqlite");
+
+        optionsBuilder
+            .UseSqlite($"Data Source={dbPath}")
+            ;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Seed();
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ DataAccessLayer/Entity/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookHub.DataAccessLayer.Entity;
+
+public class User : BaseEntity 
+{
+    [Required]
+    public string Username { get; set; }
+    
+    [Required]
+    public string Email { get; set; }
+    
+    [Required]
+    public string Password { get; set; }
+    
+    [Required]
+    public string FirstName { get; set; }
+    
+    [Required]
+    public string LastName { get; set; }
+    
+    [Required]
+    public string PhoneNumber { get; set; }
+
+    [Required]
+    public bool IsAdmin { get; set; } = false;
+}
