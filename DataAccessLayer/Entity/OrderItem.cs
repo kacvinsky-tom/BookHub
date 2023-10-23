efec1b649bb2012a02bd71c16fe65ce61fcf4603
Jakub Hlava <j.hlava@post.cz>
2023-10-23 15:19:16 +0200

    Fixed inconsistency between model and design, fixed bug where every migration updated "CreatedAt" property of all objects in database

--- DataAccessLayer/Entity/OrderItem.cs
+++ DataAccessLayer/Entity/OrderItem.cs
@@ -5,12 +5,12 @@ public class OrderItem: BaseEntity
     public int OrderId { get; set; }
     public virtual Order Order { get; set; } = null!;
 
-    public int BookId { get; set; }
+    public int? BookId { get; set; }
     public virtual Book Book { get; set; } = null!;
 
     public int Quantity { get; set; }
     
-    public int Price { get; set; }
+    public double Price { get; set; }
     
     public string Title { get; set; }
     
