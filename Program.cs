10149fea8856e198e1f8ad9e4171f3a76ca75c98
Jakub Hlava <j.hlava@post.cz>
2023-10-23 12:07:49 +0200

    Modified LoggingMiddleware, so it matches .NET standard practices

--- Program.cs
+++ Program.cs
@@ -5,12 +5,16 @@ using BookHub.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddLogging();
 
 builder.Services.AddDbContext<BookHubDbContext>();
 builder.Services.AddScoped<UnitOfWork>();
