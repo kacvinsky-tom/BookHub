77ec8fdcc498a7339fcf1ae5225c918307cb0627
Tomáš Kacvinský <xkacvin1@fi.muni.cz>
2023-11-06 13:59:41 +0100

    add Tests placeholder project

---
+++ Tests/BusinessLayer/Services/BookServiceTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.BusinessLayer.Services;
+[TestClass]
+public class BookServiceTests
+{
+    [TestMethod]
+    public void Run_PlaceHolder_IsAlwaysTrue()
+    {
+        Assert.IsTrue(true);
+    }
+
+}
