3afc78516fb714ade8f97c86f08beeb8cd378b2b
Pavel Kraus <pavel.kraus01@gmail.com>
2023-11-14 19:31:11 +0100

    Book repository refactor

---
+++ DataAccessLayer/Extension/BookDbSetExtension.cs
@@ -0,0 +1,78 @@
+﻿using DataAccessLayer.Entity;
+
+namespace DataAccessLayer.Extension;
+
+public static class BookDbSetExtension
+{
+    public static IQueryable<Book> WhereTitle(this IQueryable<Book> query, string? title)
+    {
+        if (title == null)
+        {
+            return query;
+        }
+
+        return query.Where(book => book.Title.ToLower().Contains(title.ToLower()));
+    }
+
+    public static IQueryable<Book> WhereDescription(this IQueryable<Book> query, string? description)
+    {
+        if (description == null)
+        {
+            return query;
+        }
+
+        return query.Where(book => book.Description.ToLower().Contains(description.ToLower()));
+    }
+
+    public static IQueryable<Book> WherePriceIn(this IQueryable<Book> query, int? priceFrom, int? priceTo)
+    {
+        if (priceFrom != null)
+        {
+            query = query.Where(book => book.Price >= priceFrom);
+        }
+
+        if (priceTo != null)
+        {
+            query = query.Where(book => book.Price <= priceTo);
+        }
+
+        return query;
+    }
+    
+    public static IQueryable<Book> WhereGenreIds(this IQueryable<Book> query, IEnumerable<int>? genreIds)
+    {
+        if (genreIds == null)
+        {
+            return query;
+        }
+
+        return query.Where(book => book.Genres.Any(genre => genreIds.Contains(genre.Id)));
+    }
+    
+    public static IQueryable<Book> WhereAuthorName(this IQueryable<Book> query, string? authorName)
+    {
+        if (authorName == null)
+        {
+            return query;
+        }
+
+        return query.Where(
+            book =>
+                book.Authors.Any(afn=> afn.FirstName.ToLower().Contains(authorName.ToLower()) ||
+                book.Authors.Any(aln=> aln.LastName.ToLower().Contains(authorName.ToLower()))
+        ));
+    }
+    
+    public static IQueryable<Book> WherePublisherName(this IQueryable<Book> query, string? publisherName)
+    {
+        if (publisherName == null)
+        {
+            return query;
+        }
+
+        return query.Where(
+            book =>
+                book.Publisher.Name.ToLower().Contains(publisherName.ToLower())
+        );
+    }
+}
--- DataAccessLayer/Repository/BookRepository.cs
+++ DataAccessLayer/Repository/BookRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccessLayer.Entity;
+using DataAccessLayer.Extension;
 using DataAccessLayer.Filter;
 using DataAccessLayer.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -11,69 +12,33 @@ public class BookRepository : GenericRepository<Book>, IBookRepository
     {
     }
 
-    public async Task<IEnumerable<Book>> GetWithRelations(BookFilter filterInput)
+    private IQueryable<Book> GetBasicQuery()
     {
-        var query = _context.Books
+        return _context.Books
             .Include(book => book.Genres)
             .Include(book => book.Reviews)
             .ThenInclude(review => review.User)
             .Include(book => book.Publisher)
             .Include(book => book.Authors)
             .Where(book => !book.IsDeleted);
+    }
 
-        if (filterInput.Title != null)
-        {
-            query = query.Where(book => book.Title.ToLower().Contains(filterInput.Title.ToLower()));
-        }
-
-        if (filterInput.Description != null)
-        {
-            query = query.Where(book => book.Description.ToLower().Contains(filterInput.Description.ToLower()));
-        }
-
-        if (filterInput.PriceFrom != null)
-        {
-            query = query.Where(book => book.Price >= filterInput.PriceFrom);
-        }
-
-        if (filterInput.PriceTo != null)
-        {
-            query = query.Where(book => book.Price <= filterInput.PriceTo);
-        }
-
-        if (filterInput.GenreIds != null)
-        {
-            query = query.Where(book => book.Genres.Any(genre => filterInput.GenreIds.Contains(genre.Id)));
-        }
-
-        if (filterInput.AuthorName != null)
-        {
-            query = query.Where(
-                book =>
-                    book.Authors.Any(afn=> afn.FirstName.ToLower().Contains(filterInput.AuthorName.ToLower()) ||
-                    book.Authors.Any(aln=> aln.LastName.ToLower().Contains(filterInput.AuthorName.ToLower()))
-            ));
-        }
-        if (filterInput.PublisherName != null)
-        {
-            query = query.Where(
-                book =>
-                    book.Publisher.Name.ToLower().Contains(filterInput.PublisherName.ToLower())
-            );
-        }
+    public async Task<IEnumerable<Book>> GetWithRelations(BookFilter filterInput)
+    {
+        var query = GetBasicQuery()
+            .WhereTitle(filterInput.Title)
+            .WhereDescription(filterInput.Description)
+            .WherePriceIn(filterInput.PriceFrom, filterInput.PriceTo)
+            .WhereGenreIds(filterInput.GenreIds)
+            .WhereAuthorName(filterInput.AuthorName)
+            .WherePublisherName(filterInput.PublisherName);
 
         return await query.ToListAsync();
     }
 
     public async Task<Book?> GetByIdWithRelations(int id)
     {
-        return await _context.Books
-            .Include(book => book.Genres)
-            .Include(book => book.Reviews)
-            .ThenInclude(review => review.User)
-            .Include(book => book.Publisher)
-            .Include(book => book.Authors)
-            .Where(book => !book.IsDeleted)
+        return await GetBasicQuery()
             .FirstOrDefaultAsync(book => book.Id == id);
     }
 }
