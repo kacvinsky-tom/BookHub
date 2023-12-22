026733aeccbdaabdc4a6054471d7170c677f20f5
Jakub Hlava <j.hlava@post.cz>
2023-12-22 20:18:59 +0100

    Genre list, route placeholders

---
+++ WebMVC/Areas/Admin/Controllers/GenreController.cs
@@ -0,0 +1,32 @@
+﻿using Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+public class GenreController : Controller
+{
+    private readonly GenreService _genreService;
+
+    public GenreController(GenreService genreService)
+    {
+        _genreService = genreService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _genreService.GetAll());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    public IActionResult Edit()
+    {
+        return View();
+    }
+}
---
+++ WebMVC/Areas/Admin/Controllers/PublisherController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Admin")]
+public class PublisherController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    public IActionResult Edit()
+    {
+        return View();
+    }
+}
