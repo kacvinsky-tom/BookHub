dc2216dcf8850e6a20211948b06b4904201dca1e
Jakub Hlava <j.hlava@post.cz>
2024-01-08 20:23:02 +0100

    Add john doe to admin group by default

---
+++ Migrations.Sqlite/Migrations/20240108191926_AssignCorrectAdminRole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrations.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AssignCorrectAdminRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "86718895-f083-4ba8-8452-b7a4dc9ca99c", "0d8fb324-0996-465b-a7b1-aeaaf327e6a8" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "86718895-f083-4ba8-8452-b7a4dc9ca99c", "0d8fb324-0996-465b-a7b1-aeaaf327e6a8" });
+        }
+    }
+}
