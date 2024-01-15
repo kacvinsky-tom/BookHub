f09dd4ab1e85f50c0ff5fee74e6ae5fed99f19bd
Jakub Hlava <j.hlava@post.cz>
2024-01-15 11:49:23 +0100

    various fixes for warnings across solution

--- Core/DTO/Output/SimpleListDto.cs
+++ Core/DTO/Output/SimpleListDto.cs
@@ -2,7 +2,7 @@
 
 public class SimpleListDto
 {
-    public string Id { get; set; }
+    public string Id { get; set; } = "";
 
-    public string Value { get; set; }
+    public string Value { get; set; } = "";
 }
