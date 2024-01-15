9b45d25979d235eeccf933a8add874d2c4d95ec4
Jakub Hlava <j.hlava@post.cz>
2024-01-15 19:13:47 +0100

    More changes to dockerfile structure

--- launch.sh
+++
@@ -1,4 +0,0 @@
-﻿dotnet ef database update --startup-project WebAPI --project Migrations.Sqlite
-
-dotnet out/WebMVC.dll --urls http://0.0.0.0:8800  &
-dotnet out/WebAPI.dll --urls http://0.0.0.0:8801
