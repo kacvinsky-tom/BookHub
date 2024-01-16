40c7c35085c7c1c68149fe3089c4323daf99779e
Pavel Kraus <xkraus2@fi.muni.cz>
2024-01-16 21:13:08 +0100

    Create order process

--- DataAccessLayer/Repository/Interfaces/ICartItemRepository.cs
+++ DataAccessLayer/Repository/Interfaces/ICartItemRepository.cs
@@ -7,4 +7,5 @@ public interface ICartItemRepository : IGenericRepository<CartItem>
     public Task<CartItem?> GetByIdWithRelations(int id);
     public Task<IEnumerable<CartItem>> GetAllWithRelations();
     public Task<IEnumerable<CartItem>> GetByUserIdWithRelations(int userId);
+    public Task<CartItem?> GetByUserIdAndBookId(int userId, int bookId);
 }
--- WebMVC/Areas/Shop/ViewModel/Order/OrderViewModel.cs
+++ WebMVC/Areas/Shop/ViewModel/Order/OrderViewModel.cs
@@ -1,4 +1,4 @@
-﻿namespace WebMVC.ViewModels;
+﻿namespace WebMVC.Areas.Shop.ViewModel.Order;
 
 public class OrderViewModel
 {
